572bc095051b969b3f2eedb04cc21d4bf4019562
kasper <khjo@itu.dk>
2023-06-20 15:23:59 +0000

    expand experiments

--- experiment.py
+++ experiment.py
@@ -2,19 +2,28 @@
 from pathlib import Path
 import subprocess
 import json
+import os
+import re
+import sys
 
 DEVEUIS=["70B3D57ED005E88A", "70B3D57ED005EA55", "70B3D57ED005EA56", "70B3D57ED005EA57", "70B3D57ED005EA59"]
 APPKEY="385794DDE70CE2EAB5B5B12A4807822C"
 APPEUI="0000000000000000"
 
+BOARD="b-l072z-lrwan1"
+NUM_NODES=len(DEVEUIS)
+
 SRC_PATH = Path.cwd() / "src"
 
+# create experiment
+subprocess.run(["iotlab", "experiment", "submit", "-d", "20", "-l", f"{NUM_NODES},archi=st-lrwan1:sx1276+site=saclay,,stm32Profile"],stdout=sys.stdout)
+
 # find experiment
-subprocess.run("iotlab experiment wait") #wait for experiment to start
+subprocess.run(["iotlab", "experiment", "wait"]) #wait for experiment to start
 
 #gets active experiments
-p = subprocess.run("iotlab experiment get -e", capture_output=True)
-EXPERIMENTS = json.loads(p.stdout)["Running"][0] #TODO: work with more than 1 experiment?
+p = subprocess.run(["iotlab", "experiment", "get", "-e"], capture_output=True)
+EXPERIMENT = json.loads(p.stdout)["Running"][0] #TODO: work with more than 1 experiment?
 
 
 # NODES is a list of dictionaries with following info
@@ -35,9 +44,31 @@ EXPERIMENTS = json.loads(p.stdout)["Running"][0] #TODO: work with more than 1 ex
 #     "y": "31.97",
 #     "z": "2.58"
 # }
-p = subprocess.run("iotlab experiment get -i 374209 -n", capture_output=True)
-NODES = json.loads(p.stdout)
+p = subprocess.run(["iotlab", "experiment", "get", "-i", str(EXPERIMENT), "-n"], capture_output=True)
+NODES = json.loads(p.stdout)["items"]
 
-
-for deveui in DEVEUIS:
+for deveui, node_info in zip(DEVEUIS,NODES):
     #first make binary
+    env = os.environ.copy()
+    env["BOARD"] = BOARD
+    env["DEVEUI"] = deveui
+    env["APPEUI"] = APPEUI
+    env["APPKEY"] = APPKEY
+
+    # make
+    p = subprocess.run(["make", "all"],cwd=SRC_PATH,env=env, capture_output=True)
+    
+    ## find flash file
+    p = subprocess.run(["make","info-build-json"], cwd=SRC_PATH, env=env, capture_output=True)
+    build_info = json.loads(p.stdout)
+    flash_file = Path(build_info["FLASHFILE"])
+    
+    # construct nodelist for single node
+    # might be easier with iotl"ab experiment get -d instead of -n
+    node_id = re.match('.*-(\d+)\.',node_info['network_address']).group(1)
+
+    node_string = f"{node_info['site']},{node_info['archi'].split(':')[0]},{node_id}"
+
+    # upload
+    print(node_string)
+    p = subprocess.run(["iotlab", "node", "-i", str(EXPERIMENT),  "-l", node_string, "-fl", str(flash_file)], stdout=sys.stdout)
