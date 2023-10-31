5520ee50c96f0d836593c352873baa75a28a8201
kasper <khjo@itu.dk>
2023-10-31 15:06:29 +0000

    upd. script to restart if it fails.
    This happens if all devices are turned off which happens just before all experiments are started

--- experiment_runner/resources/remote_bash.sh
+++ experiment_runner/resources/remote_bash.sh
@@ -4,4 +4,8 @@
 readonly OUTFILE="${HOME}/.iot-lab/${EXP_ID}/serial_output"
 
 echo "Launch serial_aggregator with exp_id==${EXP_ID}" >&2
-serial_aggregator -i ${EXP_ID} 2> /dev/null 1> ${OUTFILE}
+while true
+do
+    serial_aggregator -i ${EXP_ID} 2> /dev/null 1> ${OUTFILE}
+    sleep 0.1
+done
