00d306b4d7a666c2ee0a6705e5d351e0fa8b9e99
Kasper Hjort Berthelsen <khjo@itu.dk>
2023-11-21 13:37:18 +0100

    initial commit for sensor shield module

---
+++ modules/x_nucleo_iks01a3/x_nucleo_iks01a3.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2023 Kasper Hjort Berthelsen
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_st_x-nucleo-iks01a3
+ * @{
+ *
+ * @file
+ * @brief       Device driver implementation for the ST X-NUCLEO-IKS01A3
+ *
+ * @author      Kasper Hjort Berthelsen <khjo@itu.dk>
+ *
+ * @}
+ */
+
+#include "x_nucleo_iks01a3.h"
+#include "x_nucleo_iks01a3_constants.h"
+#include "x_nucleo_iks01a3_params.h"
+
+int x_nucleo_iks01a3_init(x_nucleo_iks01a3_t *dev, const x_nucleo_iks01a3_params_t *params)
+{
+
+}
