c4a415f1abeb977da3292fadae857ab177a50e39
Kasper Hjort Berthelsen <khjo@itu.dk>
2023-12-06 13:55:46 +0100

    misc changes

--- src/sensors.h
+++ src/sensors.h
@@ -8,6 +8,7 @@
 void sensors_print_available(void);
 void sensors_print_enabled(void);
 bool sensors_initialize_enabled(void);
+bool sensors_deinitialize_enabled(void);
 bool sensors_collect_into_env(Env*);
 
 #endif
