f23a7f3cd8a1f174e1b7c2bab8b7281e4b47ce78
Kasper Hjort Berthelsen <khjo@itu.dk>
2024-08-01 11:50:50 +0200

    add start and end attributes to window

--- app/terra/configuration.h
+++ app/terra/configuration.h
@@ -1,7 +1,21 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
+#include <terraprotocol.pb.h>
+#include <stdint.h>
+#include "number.h"
 
+#ifndef DISABLE_LORA 
+#include "semtech_loramac.h"
+#endif
+#ifdef DISABLE_LORA
+#define LORAWAN_APP_DATA_MAX_SIZE 242 // Since we dont have the semtech_loramac.h where it normally is
+#endif
+
+
+#ifndef MAX_WINDOW_OPERATORS
+#define MAX_WINDOW_OPERATORS 5
+#endif
 
 #define CONFIGURATION_EEPROM_START (SEMTECH_LORAMAC_EEPROM_START + SEMTECH_LORAMAC_EEPROM_LEN)
 #define CONFIGURATION_MAGIC "Terra"
@@ -9,43 +23,8 @@
 #define CONFIGURATION_LOOP_COUNTER_TYPE uint32_t
 #define CONFIGURATION_LOOP_COUNTER_SIZE sizeof(CONFIGURATION_LOOP_COUNTER_TYPE)
 #define CONFIGURATION_QUERY_MAX_SIZE TerraProtocol_Message_size
+#define CONFIGURATTION_WINDOW_DATA_SIZE 
 
-#define CONFIGURATION_LORAWAN_CONFIG_SIZE  (\
-        LORAMAC_DEVEUI_LEN +               \
-        LORAMAC_APPEUI_LEN +               \
-        LORAMAC_APPKEY_LEN +               \
-        LORAMAC_APPSKEY_LEN +              \
-        LORAMAC_NWKSKEY_LEN +              \
-        LORAMAC_DEVADDR_LEN +              \
-        LORAMAC_UPLINK_COUNTER_LEN +       \
-        LORAMAC_RX2_FREQ_LEN +             \
-        LORAMAC_RX2_DR_LEN +               \
-        sizeof(bool)) // join_state
-
-#define CONFIGURATION_TERRA_CONFIGURATION_SIZE (\
-    CONFIGURATION_LOOP_COUNTER_SIZE +\
-    CONFIGURATION_QUERY_MAX_SIZE +\
-    sizeof(uint32_t)) // raw_message_size
-
-#define CONFIGURATION_SIZE (\
-    CONFIGURATION_MAGIC_SIZE +\
-    CONFIGURATION_TERRA_CONFIGURATION_SIZE +\
-    CONFIGURATION_LORAWAN_CONFIG_SIZE)
-
-#ifndef DISABLE_LORA 
-#include "semtech_loramac.h"
-#endif
-
-#ifdef DISABLE_LORA
-#define LORAWAN_APP_DATA_MAX_SIZE 242 // Since we dont have the semtech_loramac.h where it normally is
-#endif
-
-#include <terraprotocol.pb.h>
-#include <stdint.h>
-#include "number.h"
-#ifndef MAX_WINDOW_OPERATORS
-#define MAX_WINDOW_OPERATORS 5
-#endif
 
 typedef enum _windowState {
     WINDOW_STATE_NOT_INITIALIZED,
@@ -67,12 +46,46 @@ typedef struct _WindowData {
     windowState state;
 } WindowData;
 
+/**
+ * @brief This struct contains the configuration of the Terra application
+ * These values are saved in the configuration.
+ * Note: since this is serialised and stored in flash it must not contain any pointers and be completely defined at compile-time
+ */
 typedef struct _TerraConfiguration {
     CONFIGURATION_LOOP_COUNTER_TYPE loop_counter;
     uint8_t raw_message_size;
     uint8_t raw_message_buffer[LORAWAN_APP_DATA_MAX_SIZE];
     WindowData window_data[MAX_WINDOW_OPERATORS]; // we match window_data by order of window operations. For example window_data[0] is matched to the first window in the deserialized and executed message
 } TerraConfiguration;
+
+
+#define CONFIGURATION_LORAWAN_CONFIG_SIZE  (\
+        LORAMAC_DEVEUI_LEN +               \
+        LORAMAC_APPEUI_LEN +               \
+        LORAMAC_APPKEY_LEN +               \
+        LORAMAC_APPSKEY_LEN +              \
+        LORAMAC_NWKSKEY_LEN +              \
+        LORAMAC_DEVADDR_LEN +              \
+        LORAMAC_UPLINK_COUNTER_LEN +       \
+        LORAMAC_RX2_FREQ_LEN +             \
+        LORAMAC_RX2_DR_LEN +               \
+        sizeof(bool)) // join_state
+
+/* #define CONFIGURATION_TERRA_CONFIGURATION_SIZE (\
+     CONFIGURATION_LOOP_COUNTER_SIZE +\
+     CONFIGURATION_QUERY_MAX_SIZE +\
+     sizeof(uint32_t)) // raw_message_size
+*/
+#define CONFIGURATION_TERRA_CONFIGURATION_SIZE sizeof(TerraConfiguration)
+
+#define CONFIGURATION_SIZE (\
+    CONFIGURATION_MAGIC_SIZE +\
+    CONFIGURATION_TERRA_CONFIGURATION_SIZE +\
+    CONFIGURATION_LORAWAN_CONFIG_SIZE)
+
+
+
+
 
 /**
  * @brief
